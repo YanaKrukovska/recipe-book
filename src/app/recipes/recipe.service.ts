47f94ac41f61d9e46ff1afecb49ed190204ffe52
YanaKrukovska <jana.krua@gmail.com>
2020-12-22 16:20:28 +0200

    - Saved recipes in firebase database - Added http requests to communicate with database

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,23 +1,22 @@
-import {Recipe} from './recipe.model';
 import {Injectable} from '@angular/core';
+import {Subject} from 'rxjs';
+
+import {Recipe} from './recipe.model';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list/shopping-list.service';
-import {Subject} from 'rxjs';
 
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe('Butterbrot', 'Food of Gods',
-      'https://ladyeve.ru/wp-content/uploads/2012/02/kolbasnyiy-buterbrod.jpg',
-      [new Ingredient('butter', 100), new Ingredient('bread', 1), new Ingredient('saussage', 1)]),
-    new Recipe('Butterbrot with salo', 'Less godlike but still the foot of Gods',
-      'https://znaj.ua/crops/a580aa/620x0/1/0/2019/02/19/wKf2FFhkJWVsKz8V8X25Wdd0u3ivyVWPRtcF9ziP.jpeg',
-      [new Ingredient('salo', 50), new Ingredient('bread', 1), new Ingredient('garlic', 1)])
-  ];
+  private recipes: Recipe[] = [];
 
-  constructor(private shoppingListService: ShoppingListService) {
+  constructor(private slService: ShoppingListService) {
+  }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
   }
 
   getRecipes() {
@@ -29,7 +28,7 @@ export class RecipeService {
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
-    this.shoppingListService.addIngredients(ingredients);
+    this.slService.addIngredients(ingredients);
   }
 
   addRecipe(recipe: Recipe) {
@@ -37,8 +36,8 @@ export class RecipeService {
     this.recipesChanged.next(this.recipes.slice());
   }
 
-  updateRecipe(index: number, recipe: Recipe) {
-    this.recipes[index] = recipe;
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
     this.recipesChanged.next(this.recipes.slice());
   }
 
